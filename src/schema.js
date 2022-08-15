7c07fbac53d8c9387fad5e79161d643ab9a03c32
Oscar <oscarcordero97@gmail.com>
2022-08-14 22:45:51 -0600

    update track and module schema

--- src/schema.js
+++ src/schema.js
@@ -27,8 +27,10 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
     "The number of modules this track contains"
     modulesCount: Int
     description: String
@@ -48,7 +50,9 @@ const typeDefs = gql`
   type Module {
     id: ID!
     title: String!
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    "The module's video duration, in seconds"
+    durationInSeconds: Int
   }
 `;
 
